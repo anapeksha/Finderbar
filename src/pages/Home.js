3e12266a5be0d6825da341cc535b645d85bda67a
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-30 00:42:22 +0530

    homepage made with animation

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CustomIconButton, Wrapper } from "../styles";
+
+const Home = () => {
+	const [animate, setAnimation] = useState({
+		text: "animate__animated animate__fadeInDownBig animate__slow",
+		button: "animate__animated animate__bounceInLeft animate__delay-2s",
+	});
+	const navigate = useNavigate();
+	const handleClick = () => {
+		setAnimation({
+			text: "animate__animated animate__fadeOutDownBig animate__slow animate__delay-1s",
+			button: "animate__animated animate__bounceOutRight",
+		});
+		setTimeout(() => {
+			navigate("browse");
+		}, 1750);
+	};
+	return (
+		<Wrapper>
+			<Box className={animate.text} style={{ color: "#c9cfcf" }}>
+				<Typography variant="h3">Welcome to Finderbar</Typography>
+				<Typography variant="h5" style={{ textAlign: "center" }}>
+					Click here to continue
+				</Typography>
+			</Box>
+			<Box className={animate.button}>
+				<CustomIconButton edge="start" onClick={handleClick}>
+					<ArrowForwardIcon fontSize="medium" />
+				</CustomIconButton>
+			</Box>
+		</Wrapper>
+	);
+};
+
+export default Home;
