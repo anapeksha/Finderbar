d8aec0d40d10f2d3b636d5dd185c112fe66fd2f7
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-30 23:21:33 +0530

    not found image modified

--- src/controllers/handleImage.js
+++ src/controllers/handleImage.js
@@ -1,8 +1,7 @@
 const handleImage = (image) => {
 	if (image !== null) {
 		return `https://image.tmdb.org/t/p/w500${image}`;
-	} else
-		return "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg";
+	} else return "https://www.movienewz.com/img/films/poster-holder.jpg";
 };
 
 export default handleImage;
