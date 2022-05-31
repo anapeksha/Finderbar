c4e31ff4894f26fd9a028fae39b6dd898b804fdb
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-01 00:10:59 +0530

    added bottom margin

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -1,4 +1,10 @@
-import { createTheme, Pagination, Stack, ThemeProvider } from "@mui/material";
+import {
+	Box,
+	createTheme,
+	Pagination,
+	Stack,
+	ThemeProvider,
+} from "@mui/material";
 import * as React from "react";
 
 const theme = createTheme({
@@ -20,28 +26,32 @@ const Paginate = (props) => {
 		});
 	};
 	return (
-		<Stack
-			spacing={2}
-			className="animate__animated animate__fadeIn animate__fast"
+		<Box
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				marginBottom: "2vh",
+			}}
 		>
-			<ThemeProvider theme={theme}>
-				<Pagination
-					count={props.pages || 10}
-					color="primary"
-					onChange={(e) => {
-						// @ts-ignore
-						handleChange(e.target.textContent);
-					}}
-					style={{
-						display: "flex",
-						justifyContent: "center",
-					}}
-					hideNextButton
-					hidePrevButton
-					className="custom-pagination"
-				/>
-			</ThemeProvider>
-		</Stack>
+			<Stack
+				spacing={2}
+				className="animate__animated animate__fadeIn animate__fast"
+			>
+				<ThemeProvider theme={theme}>
+					<Pagination
+						count={props.pages || 10}
+						color="primary"
+						onChange={(e) => {
+							// @ts-ignore
+							handleChange(e.target.textContent);
+						}}
+						hideNextButton
+						hidePrevButton
+						className="custom-pagination"
+					/>
+				</ThemeProvider>
+			</Stack>
+		</Box>
 	);
 };
 
