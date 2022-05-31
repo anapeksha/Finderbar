1740f5b57cf72d55565684911dc85227e81be7c2
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-01 00:02:13 +0530

    exports merged

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Paginate from "./Paginate";
+import Searchbar from "./Searchbar";
+import Cards from "./Cards";
+
+export { Navbar, Paginate, Searchbar, Cards };
---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+import handleImage from "./handleImage";
+import searchMovies from "./searchMovies";
+import trendingMovies from "./trendingMovies";
+
+export { handleImage, searchMovies, trendingMovies };
