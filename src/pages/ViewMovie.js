ca0b63f66ee711c6ad3b983945a3aabb8ede5a3c
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-04 23:17:45 +0530

    view movie page made

---
+++ src/pages/ViewMovie.js
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const ViewMovie = () => {
+	return (
+		<Box
+			style={{
+				display: "flex",
+				position: "absolute",
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				justifyContent: "center",
+				alignItems: "center",
+				color: "white",
+			}}
+		>
+			ViewMovie
+		</Box>
+	);
+};
+
+export default ViewMovie;
