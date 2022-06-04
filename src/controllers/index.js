b2739d0843ee5c4794fd32a224a965ecbe26f304
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-04 23:17:10 +0530

    easier import

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
+import handleCredits from "./handleCredits";
 import handleImage from "./handleImage";
 import searchMovies from "./searchMovies";
 import trendingMovies from "./trendingMovies";
 
-export { handleImage, searchMovies, trendingMovies };
+export { handleImage, searchMovies, trendingMovies, handleCredits };
