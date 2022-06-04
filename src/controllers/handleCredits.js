3ed3757280036f4c6fcff8571fc2f390a2359d65
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-04 23:17:01 +0530

    fetching credits controller

---
+++ src/controllers/handleCredits.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { API_KEY } from "../uri";
+
+const handleCredits = (id) => {
+	return axios(
+		`https://api.themoviedb.org/3/${id}/credits?api_key=${API_KEY}`,
+		{
+			method: "GET",
+		}
+	).then((response) => {
+		return response.data;
+	});
+};
+
+export default handleCredits;
