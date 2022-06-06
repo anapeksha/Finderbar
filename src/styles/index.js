48d3267884cfb1a77fa2a87878891ff05532b4d5
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-06 10:54:03 +0530

    custom box exported

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,6 @@
-import Wrapper from "./Wrapper";
+import CustomBox from "./CustomBox.style";
 import CustomIconButton from "./CustomIconButton.style";
 import CustomTextField from "./CustomTextField.style";
+import Wrapper from "./Wrapper";
 
-export {Wrapper, CustomIconButton, CustomTextField};
+export { Wrapper, CustomIconButton, CustomTextField, CustomBox };
