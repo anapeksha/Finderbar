a30043e176de8fa71b80918337c87ab2bd063d27
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-06 10:53:51 +0530

    custom box styled component

---
+++ src/styles/CustomBox.style.js
@@ -0,0 +1,14 @@
+import { Box, styled } from "@mui/material";
+
+const CustomBox = styled(Box)({
+	borderColor: "#282828",
+	borderStyle: "solid",
+	borderRadius: "5px",
+	borderWidth: "2px",
+	padding: "2vw",
+	backgroundColor: "#282828",
+	boxShadow:
+		"rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+});
+
+export default CustomBox;
