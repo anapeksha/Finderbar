b5e502dfcd57f6c1face39d50d4f20d80d618ad6
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-15 13:43:28 +0530

    clean export

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
+import getIMDB from "./getIMDB";
 import handleCredits from "./handleCredits";
 import handleImage from "./handleImage";
 import searchMovies from "./searchMovies";
 import trendingMovies from "./trendingMovies";
 
-export { handleImage, searchMovies, trendingMovies, handleCredits };
+export { handleImage, searchMovies, trendingMovies, handleCredits, getIMDB };
