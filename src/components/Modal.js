20a36c3bccdac179858dd40338f56cafd823fd34
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-15 11:53:02 +0530

    Modal completed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,16 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import React from "react";
+import {
+	Box,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Slide,
+	useMediaQuery,
+	useTheme,
+} from "@mui/material";
+import { Carousel } from "../components";
 import { handleImage } from "../controllers";
-var renderCount = 0;
 
 const Modal = (props) => {
 	const handleClose = () => {
@@ -22,38 +25,47 @@ const Modal = (props) => {
 				fullScreen={fullScreen}
 				open={props.open}
 				onClose={handleClose}
+				TransitionComponent={Slide}
 				aria-labelledby="responsive-dialog-title"
 			>
-				<Box
-					component="img"
-					src={handleImage(props.data.backdrop_path || props.data.poster_path)}
-					sx={{
-						height: 233,
-						width: 350,
-						maxHeight: { xs: 233, md: 167 },
-						maxWidth: { xs: 350, md: 250 },
-					}}
-				/>
-				<DialogTitle id="responsive-dialog-title">
-					{props.data.title || props.data.original_title}
-				</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						{(
-							props.data.first_air_date ||
-							props.data.release_date ||
-							"-----"
-						).substring(0, 4)}
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button autoFocus onClick={handleClose}>
-						Disagree
-					</Button>
-					<Button onClick={handleClose} autoFocus>
-						Agree
-					</Button>
-				</DialogActions>
+				<Box style={{ backgroundColor: "#252a35", color: "#c9cfcf" }}>
+					<Box
+						component="img"
+						src={handleImage(
+							props.data.backdrop_path || props.data.poster_path
+						)}
+						sx={{ display: "flex", objectFit: "contain", width: "100%" }}
+					/>
+					<DialogTitle id="responsive-dialog-title">
+						{props.data.title || props.data.original_title}
+					</DialogTitle>
+					<DialogContent>
+						<DialogContentText
+							gutterBottom
+							variant="subtitle1"
+							style={{ color: "#c9cfcf" }}
+						>
+							<strong>
+								{(
+									props.data.first_air_date ||
+									props.data.release_date ||
+									"-----"
+								).substring(0, 4)}
+							</strong>
+						</DialogContentText>
+						<DialogContentText
+							paragraph={true}
+							gutterBottom
+							variant="body2"
+							style={{ color: "#c9cfcf" }}
+						>
+							{props.data.overview}
+						</DialogContentText>
+					</DialogContent>
+					<DialogActions>
+						<Carousel id={props.data.id} />
+					</DialogActions>
+				</Box>
 			</Dialog>
 		</div>
 	);
