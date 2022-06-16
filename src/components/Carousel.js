d7e0361726b6a129fa5a26bfcdda55e060c0c3ea
anapeksha <anapeksha.mukherjee@gmail.com>
2022-06-16 23:20:45 +0530

    image size fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -24,16 +24,19 @@ const Carousel = (props) => {
 				padding: "20px",
 			}}
 		>
-			<img
+			<Box
+				component="img"
 				src={handleImage(credit.profile_path)}
 				alt={credit?.name}
 				onDragStart={handleDragStart}
 				style={{
 					borderRadius: "5px",
 					marginBottom: "5px",
+					objectFit: "contain",
 					boxShadow:
 						"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-					maxWidth: "100%",
+					width: "65px",
+					height: "90px",
 				}}
 			/>
 			<Typography variant="caption">{credit.name}</Typography>
